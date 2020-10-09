9b867f9cf0687b71b51823861916e4155f6131f2
DanielK <elder.dk@gmail.com>
2020-10-09 22:39:42 +0900

    add test signup function

---
+++ config/users/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+
+from .models import User, Expertise, Role
+
+
+class UserSignUpForm(UserCreationForm):
+    expertises = forms.ModelMultipleChoiceField(
+        queryset=Expertise.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    roles = forms.ModelMultipleChoiceField(
+        queryset=Role.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save()
+        user.expertise.add(*self.cleaned_data.get('expertises'))
+        user.role.add(*self.cleaned_data.get('roles'))
+        return user
