2e775837c0bc23f4c18c06aafd73d4977c4d45fd
DanielK <elder.dk@gmail.com>
2020-10-10 16:04:01 +0900

    fix Procfile app name issue

--- config/config/settings.py
+++ config/config/settings.py
@@ -123,8 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 AUTH_USER_MODEL = 'users.User'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap'
