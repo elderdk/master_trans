ffc8eb9268d2baa7d3b56a8e109600fd8ad5a9f5
DanielK <elder.dk@gmail.com>
2020-10-26 22:59:59 +0900

    add ctrl up down shortcuts

--- translation/static/translation/js/textarea.js
+++ translation/static/translation/js/textarea.js
@@ -25,24 +25,7 @@ function postloadautosize(el){
   },0);
 }
 
-// Textarea navigation and commit shortcuts
-
-// Commit (Ctrl + Enter)
-function make_commit(e, value) {
-  if (e.ctrlKey && e.keyCode == 13) {
-      // call your function to do the thing
-      commit_url = e.currentTarget.attributes.getNamedItem("commit-url").value  
-      text = e.currentTarget.value
-      status_tag = e.target.parentNode.previousElementSibling
-      status_tag.innerHTML = 'Translated'
-      send_commit(commit_url, csrf_token, text)
-  }
-}
-// register the handler
-textareas.forEach(textarea => 
-  textarea.addEventListener('keyup', make_commit, false)
-  )
-
+// make commit
 function send_commit(commit_url, csrf_token, text){
   var xhr = new XMLHttpRequest();
   xhr.open("POST", commit_url, true);
@@ -50,3 +33,47 @@ function send_commit(commit_url, csrf_token, text){
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(text);
 }
+
+//// textarea shortcuts
+
+// Commit (ctrl + enter)
+function make_commit(e) {
+  commit_url = e.currentTarget.attributes.getNamedItem("commit-url").value  
+  text = e.currentTarget.value
+  status_tag = e.target.parentNode.previousElementSibling
+  status_tag.innerHTML = 'Translated'
+  send_commit(commit_url, csrf_token, text)
+  move_below(e)
+}
+
+// Move down (ctrl + arrowdown)
+function move_below(e) {
+  textarea_id = e.currentTarget.id;
+  next_id = "#target-" + (parseInt(textarea_id.split("-")[1]) + 1)
+  next_textarea = document.querySelector(next_id)
+  next_textarea.focus()
+  setTimeout(function(){ next_textarea.selectionStart = next_textarea.selectionEnd = 10000; }, 0);
+}
+
+// Move up (ctrl + arrowup)
+function move_up(e) {
+  textarea_id = e.currentTarget.id;
+  previous_id = "#target-" + (parseInt(textarea_id.split("-")[1]) - 1)
+  previous_textarea = document.querySelector(previous_id)
+  previous_textarea.focus()
+  setTimeout(function(){ previous_textarea.selectionStart = previous_textarea.selectionEnd = 10000; }, 0);
+}
+
+function short_cuts(e) {
+  // Ctrl + Enter
+  if (e.ctrlKey && e.keyCode == 13) { make_commit(e) }
+  // Ctrl + Downarrow
+  else if (e.ctrlKey && e.keyCode == 40) { move_below(e) }
+  // Ctrl + Uparrow
+  else if (e.ctrlKey && e.keyCode == 38) { move_up(e) }
+}
+// register the handler
+textareas.forEach(textarea => 
+  textarea.addEventListener('keyup', short_cuts, false)
+  )
+
